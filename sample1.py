e3928467bb22ce501e0e9926a96f8da0b3e68c0a
Sebastian Cheung <sebastian_cheung@yahoo.com>
2017-09-22 01:43:37 +0100

    refactored code into function

--- sample1.py
+++ sample1.py
@@ -7,30 +7,36 @@ Azure Python SDK documentation : https://aka.ms/azure-python-sdk
 
 # Emotion analysis
 
-########### Python 2.7 #############
 import httplib, urllib, base64
 
-headers = {
-    # Request headers. Replace the placeholder key below with your subscription key.
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': 'e257b9e881914b878039b41fc101e870'
-}
 
-params = urllib.urlencode({
-})
+def method_name():
+    headers = {
+        # Request headers. Replace the placeholder key below with your subscription key.
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': 'e257b9e881914b878039b41fc101e870'
+    }
 
-# Replace the example URL below with the URL of the image you want to analyze.
-body = "{ 'url': 'http://dreamatico.com/data_images/face/face-1.jpg' }"
+    params = urllib.urlencode({
 
-try:
-    # NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
-    #   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the
-    #   URL below with "westcentralus".
-    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    })
+
+    # Replace the example URL below with the URL of the image you want to analyze.
+    body = "{ 'url': 'http://dreamatico.com/data_images/face/face-1.jpg' }"
+
+    try:
+        # NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
+        #   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the
+        #   URL below with "westcentralus".
+        conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+        conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
+        response = conn.getresponse()
+        data = response.read()
+        print(data)
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+
+if __name__ == '__main__':
+    method_name()
